1fe6045eea84afe166efeecba30d706066f5f348
Rubylenshy <reztomoloju@gmail.com>
2022-12-21 23:04:51 +0100

    card components plug-in added

---
+++ components-with-mouse-event/assets/js/main.js
@@ -0,0 +1,23 @@
+
+const container = document.querySelector('.container')
+cards('./cards.json')
+
+async function cards(file) {
+    let obj1 = await fetch(file);
+    let jsonCards = await obj1.text();
+
+    let cards = JSON.parse(jsonCards);
+
+    var newCards = ''
+    cards.forEach(item => {
+        let htmlCards = `<div class="card-component">
+                            <img src="${item.url}" alt="">
+                            <h1>${item.heading}</h1>
+                            <p>${item.paragragh}</p>
+                        </div>`;
+        newCards += htmlCards;
+
+    });
+
+    container.innerHTML = newCards
+}
