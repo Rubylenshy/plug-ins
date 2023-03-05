4c5c8b9b6b90c3b9a605dac28b9836ff64ce7848
Rubylenshy <reztomoloju@gmail.com>
2023-03-05 22:34:10 +0100

    flip effect added

--- credit-card-switchup/assets/js/main.js
+++ credit-card-switchup/assets/js/main.js
@@ -2,13 +2,19 @@ const cardOne = document.querySelector('.one')
 const cardOneDetails = document.querySelector('.one-details')
 
 cardOne.addEventListener('click', ()=>{
-    cardOne.style.transform = 'rotateY(180deg)'
-    // cardOne.style.display = 'none'
-    // cardOneDetails.style.display = 'block'
-    // cardOne.style.animation = 'bounce 1s'
+    cardOne.classList.add('flip')
+    setTimeout(() => {
+        cardOne.style.display = 'none'
+        cardOneDetails.style.display = 'block'
+        cardOneDetails.classList.remove('flip')
+    }, 300);
+
 })
-cardOneDetails.addEventListener('click', ()=>{
-    cardOneDetails.style.transform = 'rotateY(-180deg)'
-    // cardOneDetails.style.display = 'none'
-    // cardOne.style.display = 'block'
+document.querySelector('.see-card-btn').addEventListener('click', ()=>{
+    cardOneDetails.classList.add('flip')
+    setTimeout(() => {
+        cardOneDetails.style.display = 'none'
+        cardOne.style.display = 'block'
+        cardOne.classList.remove('flip')
+    }, 300);
 })
