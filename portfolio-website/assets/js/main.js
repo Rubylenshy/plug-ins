af9dd2f4b2a62075105743b780d82481be1bd594
Rubylenshy <reztomoloju@gmail.com>
2023-03-09 21:38:40 +0100

    illustration defined

--- portfolio-website/assets/js/main.js
+++ portfolio-website/assets/js/main.js
@@ -1,6 +1,8 @@
 
 const hamburger_menu = document.querySelector('.hamburger')
+const navigation_menu = document.querySelector('.navigation-menu')
 
 hamburger_menu.addEventListener('click', ()=>{
     hamburger_menu.classList.toggle('active')
+    navigation_menu.classList.toggle('active')
 })
